f64e7f875c91f5e492a2d919f97ccd3c10f2583c
Yan Chen <ychen4931@gmail.com>
2026-05-13 20:22:42 -0400

    Fix cloudinary collectstatic settings

--- searchwilds/settings.py
+++ searchwilds/settings.py
@@ -161,9 +161,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
+STATICFILES_STORAGE = "django.contrib.staticfiles.storage.StaticFilesStorage"
 WHITENOISE_MANIFEST_STRICT = False
 
 # FOR DEPLOYMENT 
